d2884e54726729d4e0b29aa9e9b3528eb18a40e9
Javi <javi@traity.com>
2015-02-08 13:48:45 +0100

    Update gitignore to update spikes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,12 @@
 function Config() {
-  this.mongo_url = process.env.MONGO_URL || 'mongodb://localhost/trains_test';
-  this.redis_url = process.env.REDIS_URL || 'redis://localhost:6379/0';
+  this.mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/trains_test';
+  this.redisUrl = process.env.REDIS_URL || 'redis://localhost:6379/0';
+  this.mailer = {
+    host: process.env.SMTP_HOST || '127.0.0.1',
+    port: process.env.SMTP_PORT ||  1025,
+    username: process.env.SMTP_USERNAME,
+    password: process.env.SMTP_PASSWORD
+  }
 };
 
 module.exports = new Config();
